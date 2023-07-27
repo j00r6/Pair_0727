8b70006f5ed756b292a4f23af0f83ff3249d23e4
j00r6 <jindory8@gmail.com>
2023-07-27 14:14:10 +0900

    first commit

---
+++ src/test/java/com/example/pair_0727/Pair0727ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pair_0727;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pair0727ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
